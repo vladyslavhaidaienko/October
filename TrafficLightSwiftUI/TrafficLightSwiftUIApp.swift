9ad74f5e1ef04270095e01e6ea7957ebe49ba0c2
vladyslavhaidaienko <gvgnexus5@gmail.com>
2021-10-26 12:19:17 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Vladyslav Haidaienko on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
